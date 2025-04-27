f2158a1a2f9881b80341bf068d85e40a003f2d20
Varun2365Kumar <varun2365scifi@gmail.com>
2025-04-27 12:08:25 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,10 @@
+const express = require('express')
+const os = require('os')
+const app = express();
+app.listen(8000, ()=>{
+    console.log("App Listening on port 8000")
+})
+
+app.get('/', (req, res)=>{
+    res.send("VPS STARTED " + os.cpus()[0]['model'] + " " +os.cpus().length);
+})
